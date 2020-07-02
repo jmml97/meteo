9221ce9516589f8201600c57eff6556ea1c56c81
José María Martín <jmmartinluque@gmail.com>
2020-07-02 12:29:30 +0200

    Initial Commit

---
+++ Shared/MeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoApp.swift
+//  Shared
+//
+//  Created by José María Martín Luque on 02/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
