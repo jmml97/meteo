2425910cb41b17c78979e3a4b06802d46636978e
José María Martín <jmmartinluque@gmail.com>
2020-07-06 17:56:52 +0200

    Cuadro de búsqueda para macOS

--- Shared/SearchBar.swift
+++ Shared/SearchBar.swift
@@ -7,6 +7,45 @@
 
 import SwiftUI
 
+#if os(macOS)
+
+struct SearchBar: NSViewRepresentable {
+    
+    @Binding var text: String
+    var placeholder: String
+    
+    class Coordinator: NSObject, NSSearchFieldDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func controlTextDidChange(_ notification: Notification) {
+            guard let searchBar = notification.object as? NSSearchField else { return }
+            text = searchBar.stringValue
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeNSView(context: NSViewRepresentableContext<SearchBar>) -> NSSearchField {
+        let searchField = NSSearchField(frame: .zero)
+        searchField.delegate = context.coordinator
+        searchField.placeholderString = placeholder
+        return searchField
+    }
+    
+    func updateNSView(_ nsView: NSSearchField, context: NSViewRepresentableContext<SearchBar>) {
+        nsView.stringValue = text
+    }
+}
+
+#else
+
 struct SearchBar: UIViewRepresentable {
 
     @Binding var text: String
@@ -42,3 +81,5 @@ struct SearchBar: UIViewRepresentable {
         uiView.text = text
     }
 }
+
+#endif
