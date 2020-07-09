4b7de7d5ae912743f11b4dab2a622e76c2865d44
José María Martín <jmmartinluque@gmail.com>
2020-07-09 17:07:11 +0200

    Reorganiza vistas, mejora comentario

--- Shared/PredictionView.swift
+++ Shared/PredictionView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// MARK: Constants
+
 let weatherIcons: [AEMETDescripcion: String] = [
     .despejado: "sun.max",
     .cubierto: "cloud",
@@ -16,6 +18,10 @@ let weatherIcons: [AEMETDescripcion: String] = [
 
 let isoDateFormatString = "yyyy-MM-dd'T'HH:mm:ss"
 
+
+// MARK: - Helper functions
+
+/// Returns a string in the desired format from a string in another format
 func getFormattedDateFromString(dateString: String, inFormat: String, outFormat: String) -> String {
     
     let inFormatter = DateFormatter()
@@ -30,6 +36,10 @@ func getFormattedDateFromString(dateString: String, inFormat: String, outFormat:
     return outFormatter.string(for: date)!
 }
 
+// MARK: - Views
+
+/// Main prediction view
+/// - Parameter townID: id of the town this view represents, needed to pull data
 struct PredictionView: View {
     
     @StateObject var loader = PredictionDataLoader()
@@ -39,21 +49,12 @@ struct PredictionView: View {
     var body: some View {
         if let hourlyPredictions = loader.hourlyPredictionsContainer {
             VStack(alignment: .leading) {
-                HStack {
-                    Text(hourlyPredictions.provincia)
-                    Text("Elaborado: \(getFormattedDateFromString(dateString: hourlyPredictions.elaborado, inFormat: isoDateFormatString, outFormat: "MM/dd/yyyy HH:mm"))")
-                }
+                MetadataView(province: hourlyPredictions.provincia, predictionDate: hourlyPredictions.elaborado)
                 .padding(10)
-                Text("Predicción horaria")
-                    .font(.title)
-                    .padding(10.0)
                 HourlyPredictionView(predictions: hourlyPredictions).navigationTitle(hourlyPredictions.nombre)
                 Spacer()
                 if let dailyPredictions = loader.dailyPredictionsContainer {
-                    Text("Predicción diaria")
-                        .font(.title)
-                        .padding(10.0)
-                    DailyPredictionView(predictions: dailyPredictions)
+                    DailyPredictionListView(predictions: dailyPredictions)
                 }
             }
         } else {
@@ -65,107 +66,106 @@ struct PredictionView: View {
     }
 }
 
+struct MetadataView: View {
+    
+    let province, predictionDate: String
+    
+    var body: some View {
+        HStack {
+            Text(province)
+            Text("Elaborado: \(getFormattedDateFromString(dateString: predictionDate, inFormat: isoDateFormatString, outFormat: "MM/dd/yyyy HH:mm"))")
+        }
+    }
+}
+
 struct HourlyPredictionView: View {
     
     let predictions: AEMETHourlyPredictionContainer
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 20) {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Hora")
-                    Text("Cielo")
-                    Text("Temperatura")
-                }
-                ForEach(predictions.prediccion.dia, id:\.fecha) { d in
-                    Text(getFormattedDateFromString(dateString: d.fecha, inFormat: isoDateFormatString, outFormat: "d MMM"))
-                    let datosHorarios = Array(zip(d.temperatura, d.estadoCielo))
-                    ForEach(datosHorarios, id:\.0.periodo) { dato in
-                        HourlyDataView(periodo: dato.0.periodo, temp: dato.0.value, estadoCielo: dato.1.descripcion)
+        VStack {
+            Text("Predicción horaria")
+                .font(.title)
+                .padding(10.0)
+            ScrollView(.horizontal) {
+                HStack(spacing: 20) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Hora")
+                        Text("Cielo")
+                        Text("Temperatura")
+                    }
+                    ForEach(predictions.prediccion.dia, id:\.fecha) { d in
+                        Text(getFormattedDateFromString(dateString: d.fecha, inFormat: isoDateFormatString, outFormat: "d MMM"))
+                        let datosHorarios = Array(zip(d.temperatura, d.estadoCielo))
+                        ForEach(datosHorarios, id:\.0.periodo) { dato in
+                            HourlyDataView(period: dato.0.periodo, temp: dato.0.value, sky: dato.1.descripcion)
+                        }
                     }
                 }
-            }
-        }.padding(.leading, 10.0)
+            }.padding(.leading, 10.0)
+        }
     }
 }
 
 struct HourlyDataView: View {
     
-    let periodo, temp: String
-    let estadoCielo: AEMETDescripcion
+    let period, temp: String
+    let sky: AEMETDescripcion
     
     var body: some View {
         VStack(spacing: 10) {
-            Text(periodo + "h")
-            Image(systemName: weatherIcons[estadoCielo, default: "tornado"])
+            Text(period + "h")
+            Image(systemName: weatherIcons[sky, default: "tornado"])
             Text(temp + "º")
         }
     }
 }
 
-//struct PredictionView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PredictionView(townID: "29718")
-//    }
-//}
-
-struct DailyPredictionView: View {
+/// List of each one of the daily predictions
+struct DailyPredictionListView: View {
     
     let predictions: AEMETDailyPredictionContainer
     
     var body: some View {
-        List(predictions.prediccion.dia, id:\.fecha) { d in
-            HStack {
-                Text(getFormattedDateFromString(dateString: d.fecha, inFormat: isoDateFormatString, outFormat: "d MMM"))
-                Text("mín: " + String(d.temperatura.minima))
-                Text("máx: " + String(d.temperatura.maxima))
+        VStack {
+            Text("Predicción diaria")
+                .font(.title)
+                .padding(10.0)
+            List {
+                ForEach(predictions.prediccion.dia, id:\.fecha) { d in
+                    DailyPredictionView(date: d.fecha, min: String(d.temperatura.minima), max: String(d.temperatura.maxima))
+                }
             }
         }
+    }
+}
+
+struct DailyPredictionView: View {
+    
+    let date, min, max: String
+    
+    var body: some View {
+        HStack {
+            Text(getFormattedDateFromString(dateString: date, inFormat: isoDateFormatString, outFormat: "d MMM"))
+            Text("mín: " + String(min))
+            Text("máx: " + String(max))
+        }
+    }
+    
+}
+
+// MARK: - Previews
+
+struct MetadataView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MetadataView(province: "Arribacete", predictionDate: "2020-07-06T00:00:00")
     }
 }
 
 struct DailyPredictionView_Previews: PreviewProvider {
     
     static var previews: some View {
-        DailyPredictionView(
-            predictions: AEMETDailyPredictionContainer(
-                elaborado: "AEMET",
-                nombre: "Villabajo",
-                provincia: "Abajo",
-                prediccion: AEMETDailyPrediction(dia: [AEMETDailyDayData(
-                    probPrecipitacion: [AEMETProbPrecipitacion(value: 50, periodo: "0-24")],
-                    cotaNieveProv: [AEMETCotaNieveProv(value: "700", periodo: "0-24")],
-                    estadoCielo: [AEMETEstadoCielo(value: "", periodo: "0-24", descripcion: .despejado)],
-                    viento: [AEMETDailyWind(direccion: "S", velocidad: 20, periodo: "0-24")],
-                    rachaMax: [AEMETCotaNieveProv(value: "39", periodo: "0-24")],
-                    temperatura: AEMETHumedadRelativa(maxima: 30, minima: 10, dato: [AEMETDato(value: 25, hora: 15)]),
-                    sensTermica: AEMETHumedadRelativa(maxima: 30, minima: 10, dato: [AEMETDato(value: 25, hora: 15)]),
-                    humedadRelativa: AEMETHumedadRelativa(maxima: 30, minima: 10, dato: [AEMETDato(value: 25, hora: 15)]),
-                    uvMax: 10,
-                    fecha: "2020-07-06T00:00:00"
-                ),
-                AEMETDailyDayData(
-                    probPrecipitacion: [AEMETProbPrecipitacion(value: 50, periodo: "0-24")],
-                    cotaNieveProv: [AEMETCotaNieveProv(value: "700", periodo: "0-24")],
-                    estadoCielo: [AEMETEstadoCielo(value: "", periodo: "0-24", descripcion: .despejado)],
-                    viento: [AEMETDailyWind(direccion: "S", velocidad: 20, periodo: "0-24")],
-                    rachaMax: [AEMETCotaNieveProv(value: "39", periodo: "0-24")],
-                    temperatura: AEMETHumedadRelativa(maxima: 34, minima: 16, dato: [AEMETDato(value: 25, hora: 15)]),
-                    sensTermica: AEMETHumedadRelativa(maxima: 30, minima: 10, dato: [AEMETDato(value: 25, hora: 15)]),
-                    humedadRelativa: AEMETHumedadRelativa(maxima: 30, minima: 10, dato: [AEMETDato(value: 25, hora: 15)]),
-                    uvMax: 10,
-                    fecha: "2020-07-07T00:00:00"
-                )
-                ]),
-                id: 1,
-                version: 1,
-                origen: AEMETSource(productor: "AEMET",
-                                    web: URL(string: "aemet.es")!,
-                                    enlace: URL(string: "aemet.es")!,
-                                    notaLegal: URL(string: "aemet.es")!,
-                                    language: "es",
-                                    copyright: "AEMET")
-            )
-        )
+        DailyPredictionView(date: "2020-07-06T00:00:00", min: "19", max: "34")
     }
 }
